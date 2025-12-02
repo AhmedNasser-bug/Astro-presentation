94243afd9e234790268ea42e8a6b7712866349bb
Ahmed Nasser Mohammed <enjoyplayax@gmail.com>
2025-12-02 09:56:39 +0200

    Fixed deploymrnt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
@@ -6,8 +5,6 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  adapter: vercel(),
-  output: 'server',
   integrations: [
     react(), 
     vue(), 
